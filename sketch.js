5873c61077fba342e219619674828447e34ba7d6
さんたま <115996781+saintmaria000@users.noreply.github.com>
2025-05-30 17:58:47 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let sound;
+let fft;
+let button;
+
+function preload() {
+  sound = loadSound('music/4.mp3');  // 音源ファイルのパス
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fft = new p5.FFT();
+  
+  // ボタンクリックイベントの設定
+  button = select('#toggle-btn');
+  button.mousePressed(togglePlay);
+  
+  noFill();
+  stroke(255);
+  strokeWeight(2);
+}
+
+function draw() {
+    background(0, 80); // 残像効果（トレイル演出）
+  
+    let waveform = fft.waveform();
+  
+    beginShape();
+    for (let i = 0; i < waveform.length; i++) {
+      let x = map(i, 0, waveform.length, 0, width);
+      let y = map(waveform[i], -1, 1, 0, height);
+      vertex(x, y);
+    }
+    endShape();
+  }
+
