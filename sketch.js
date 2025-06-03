c41308023b497cb5280d6b984455ca2f8efe27a1
さんたま <115996781+saintmaria000@users.noreply.github.com>
2025-06-03 22:14:40 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,23 +3,31 @@ let fft;
 let button;
 let volumeSlider;
 
+function preload() {
+  // 最初にデフォルトの音を読み込む
+  sound = loadSound('sound.mp3');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fft = new p5.FFT();
 
   volumeSlider = select("#volume-slider");
 
+  // デフォルトサウンドをFFTにセット
+  fft.setInput(sound);
+
+  // ファイル読み込みのイベント
   document.getElementById("file-input").addEventListener("change", (e) => {
-    if (sound) {
+    if (sound && sound.isPlaying()) {
       sound.stop();
     }
 
     const file = e.target.files[0];
     if (file) {
       sound = loadSound(URL.createObjectURL(file), () => {
-        console.log("Sound loaded");
-
-        fft.setInput(sound); // FFTに入力をセット（リバーブ削除）
+        console.log("User Sound loaded");
+        fft.setInput(sound);
       });
     }
   });
@@ -36,7 +44,7 @@ function draw() {
   background(0, 20); // 残像効果
 
   if (sound && sound.isLoaded()) {
-    let waveform = fft.waveform(); // fft.getEnergy() などでも可
+    let waveform = fft.waveform();
 
     beginShape();
     for (let i = 0; i < waveform.length; i++) {
@@ -60,7 +68,7 @@ function togglePlay() {
   } else {
     sound.setVolume(0);
     sound.play();
-    fft.setInput(sound); // 念のためここでも設定しておく
+    fft.setInput(sound);
     sound.setVolume(1, 1);
   }
 }
